8876c19852b1e41f6aa71432db07cb60916d0db9
FlavioKde <flaviodavirro@gmail.com>
2025-11-01 17:00:05 +0100

    feat: add Role and RoleType entity, and AuthRequestDTO and RegisterRequestDTO as records
    chore: remove old RegisterRequestDTO class (replaced by record-based version)

---
+++ barrisense-feedback-api/src/main/java/com/barriSenseBack/barrisense_feedback_api/dto/AuthRequestDTO.java
@@ -0,0 +1,5 @@
+package com.barriSenseBack.barrisense_feedback_api.dto;
+
+
+public record AuthRequestDTO(String username, String password) {
+}
---
+++ barrisense-feedback-api/src/main/java/com/barriSenseBack/barrisense_feedback_api/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.barriSenseBack.barrisense_feedback_api.dto;
+
+public record RegisterRequestDTO(String username, String password, String email) {
+}
